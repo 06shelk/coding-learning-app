705fa503ae6de0cf91b856f6380d67445c14ad74
06shelk <shlee4349@gmail.com>
2024-09-19 00:37:05 +0900

    login, signup tnwjdalc global css 색상 변경

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import Header from '../components/Header'
-import '../css/Login.css'
+import styles from '../css/Login.module.css'
 
 
-function LoginForm () {
+function LoginForm() {
     return (
-        <>
-            <div id='LoginForm'>
-                <form className='LoginForm__inner'>
-                    <div className='form__con'>
-                        <p>이메일</p>
-                        <input id='userid' type='text' placeholder='이메일을 입력해주세요'/>
-                    </div>
-                    <div className='form__con'>
-                        <p>비밀번호</p>
-                        <input id='pwd' type='password' maxlength='5' placeholder='비밀번호를 입력해 주세요'/>
-                    </div>
-                  
-                    <input className ="signBtn" type='submit' value='로그인'/>
-                    <p className='pwFind'>비밀번호 찾기</p>
-                </form>
-            </div>
-        </>
-    )
-}
+      <div id={styles.LoginForm}>
+        <form className={styles.LoginForm__inner}>
+          <div className={styles.form__con}>
+            <p>이메일</p>
+            <input id='userid' type='text' placeholder='이메일을 입력해주세요' />
+          </div>
+          <div className={styles.form__con}>
+            <p>비밀번호</p>
+            <input
+              id='pwd'
+              type='password'
+              maxLength='5'
+              placeholder='비밀번호를 입력해 주세요'
+            />
+          </div>
+  
+          <input className={styles.loginBtn} type='submit' value='로그인' />
+          <p className={styles.pwFind}>비밀번호 찾기</p>
+        </form>
+        <div className={styles.notMember}>아직 회원이 아니신가요? <span> 회원가입</span></div>
+      </div>
+    );
+  }
 
 const Login = () => {
   return (
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,36 +1,33 @@
 import React from 'react'
 import Header from '../components/Header'
-import '../css/SignUp.css'
+import styles from '../css/SignUp.module.css';
 
 
-function SignUpForm () {
+function SignUpForm() {
     return (
-        <>
-            <div id='signUpForm'>
-                <form className='signUpForm__inner'>
-                    <div className='form__con'>
-                        <p>이메일</p>
-                        <input id='userid' type='text' placeholder='이메일을 입력해주세요'/>
-                    </div>
-                    <div className='form__con'>
-                        <p>비밀번호</p>
-                        <input id='pwd' type='password' maxlength='5' placeholder='비밀번호를 입력해 주세요'/>
-                    </div>
-                    <div className='form__con'>
-                        <p>비밀번호 확인</p>
-                        <input id='pwd' type='password' maxlength='5' placeholder='비밀번호를 입력해 주세요'/>
-                    </div>
-                    <div className='form__con'>
-                        <p>이름</p>
-                        <input id='pwd' type='text' maxlength='5' placeholder='이릉을 입력해 주세요'/>
-                    </div>
-                    <input className ="signBtn" type='submit' value='회원가입'/>
-                </form>
-            </div>
-        </>
-    )
-}
-
+      <div id={styles.signUpForm}>
+        <form className={styles.signUpForm__inner}>
+          <div className={styles.form__con}>
+            <p>이메일</p>
+            <input id='userid' type='text' placeholder='이메일을 입력해주세요' />
+          </div>
+          <div className={styles.form__con}>
+            <p>비밀번호</p>
+            <input id='pwd' type='password' maxLength='5' placeholder='비밀번호를 입력해 주세요' />
+          </div>
+          <div className={styles.form__con}>
+            <p>비밀번호 확인</p>
+            <input id='pwdConfirm' type='password' maxLength='5' placeholder='비밀번호를 입력해 주세요' />
+          </div>
+          <div className={styles.form__con}>
+            <p>이름</p>
+            <input id='name' type='text' maxLength='5' placeholder='이름을 입력해 주세요' />
+          </div>
+          <input className={styles.signBtn} type='submit' value='회원가입' />
+        </form>
+      </div>
+    );
+  }
 const SignUp = () => {
   return (
     <>
